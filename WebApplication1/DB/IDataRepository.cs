4bb7c5d0742451077cf5d993395771aeaa416adf
filon13p <filon13p@gmail.com>
2021-05-10 16:09:39 +0300

    интерфейс

---
+++ WebApplication1/DB/IDataRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public interface IDataRepository
+    {
+
+        IEnumerable<Models.Tarif> getTarifs();
+        IEnumerable<Models.Tarif> getTarifsFlat();
+        void  createRequest(Models.Request);
+    }
+}
